bfa1f7a885d379b704ca6bdb843dfdbc066a7089
anushkapundir1 <anushka.pundir10@gmail.com>
2025-08-17 23:39:51 +0530

    Created user profile model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile  # Importing UserProfile model
+
+admin.site.register(UserProfile)  # Registering UserProfile model with the admin site
 
 # Register your models here.
--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User  # Importing User model for user-related fields
 # Create your models here.
+
+class UserProfile(models.Model): # User profile model to extend User functionality
+    user= models.OneToOneField(User, on_delete=models.CASCADE)  # One-to-one relationship with User
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profiles/', default='profiles/default.png', blank=True, null=True)
+    bio= models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+    
